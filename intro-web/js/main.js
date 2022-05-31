b47d29c37ff193523272561c2e2ac6bb129d0e13
Francisco Campos <hey@francampos.me>
2022-05-30 23:45:34 -0600

    codigo de la primera clase

---
+++ intro-web/js/main.js
@@ -0,0 +1,83 @@
+
+// alert("Hola")
+
+
+// document.write("Hola");
+
+// Imprimir por consola
+// console.log("Hola desde consola");
+// console.log(5+3)
+// console.log(4 > 3)
+
+
+// Imprimir en nuestra pagina web
+// document.write("Hola en nuestra pagina web desde Javascript");
+
+// Imprimir mediante una alerta emergente
+// alert("Hola desde alert");
+
+// variables
+
+// Pedir datos a el usuario
+/*
+*/
+// var nombreDeUsuario = prompt("Ingrese su nombre")
+
+
+// document.write("Hola " + nombreDeUsuario);
+// console.log("Hola, como estas " + nombreDeUsuario)
+
+// console.log("Hola, como estas " + nombreDeUsuario)
+
+// console.log("Hola, "+ prompt("Cual es tu nombre"))
+
+// variable de tipo string
+var nombreDeUsuario = "Francisco"
+
+// Variable de tipo Number
+var tipoNumber = 2;
+var tipoDouble = 2.2;
+
+// tipo de dato Boolean
+var tipoBoolean = false;
+var tipoBooleanTrue = true;
+
+var tipoNull = null;
+
+var tipoUndefined;
+
+// console.log(tipoUndefined)
+
+var tipoObjecto = {
+  nombre: "Francisco",
+  apellido: "Campos",
+  edad: 27
+}
+
+// {}  llaves
+// [] corchete
+// () parantesis
+
+// console.log(tipoObjecto)
+
+var numero1 = prompt("Ingrese el primer numero a sumar?")
+var numero2 = prompt("Ingrese el segundo numero a sumar?")
+var suma = numero1 + numero2;
+document.write("Suma de " + numero1 +" + " + numero2 +" es: " + suma)
+
+
+
+var resta = 3 - 2;
+console.log("Resta de 3 - 2 es :", resta)
+var multiplicacion = 3 * 5;
+console.log("Multiplicacion de 3 * 5 es:", multiplicacion)
+var division = 4 / 2
+console.log("Division de 4 / 2 es:", division)
+var modulo = 4 % 2
+console.log("Modulo de 4 / 2 es:", modulo)
+var exponencial = 2**2
+console.log("Exponencial de 2**2 es:", exponencial)
+
+
+
+
